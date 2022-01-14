7f114fc6bc9d943583bd71637a7b11c1aa6ee637
KostyarikX <94854240+KostyarikX@users.noreply.github.com>
2022-01-14 11:25:51 +0300

    Update reforged.jsx

--- reforged.jsx
+++ reforged.jsx
@@ -11,11 +11,9 @@ PathOut = dlg.add('edittext', {x:90, y:50, width:400,height:25} ,'');
 btn_inputFolder = dlg.add('button',{x:495, y:20, width:50,height:25},'...');  
 btn_outputFolder = dlg.add('button',{x:495, y:50, width:50,height:25},'...');  
 
-
 CkeckAdd = dlg.add('checkbox',{x:15, y:80, width:93,height:25},'Generate PSDs'); 
 btn_import = dlg.add('button',{x:15, y:110, width:207,height:25},'Convert'); 
 
-/////////////////////////////////
 CkeckAdd.onClick = function (){  
 if(this.value){  
      	savePsd = true;
@@ -24,19 +22,16 @@ else{
        	savePsd = false;
       }  
 } 
-///////////////////////////////
-//var myFile = myFile
-btn_inputFolder.onClick = function(){  
-	//myFile = File.openDialog ('Select a sogo file', false);  
+
+btn_inputFolder.onClick = function(){    
 	var inputFolder = Folder.selectDialog("Select an input folder");
 	if(inputFolder!=null){PathIn.text = inputFolder.fsName}; 
 }; 
-btn_outputFolder.onClick = function(){  
-	//myFile = File.openDialog ('Select a sogo file', false);  
+btn_outputFolder.onClick = function(){   
 	var outputFolder = Folder.selectDialog("Select an output folder");
 	if(outputFolder!=null){PathOut.text = outputFolder.fsName};
 }; 
-/////////////////////////////
+
 btn_import.onClick = function(){  
   dlg.close();
   runthis();
@@ -49,7 +44,6 @@ dlg.show();
 
 function runthis()
 {
-	//alert(outputFolder.fsName);
 	var cWhite = new SolidColor();  
 		cWhite.rgb.red = 255;  
 		cWhite.rgb.green = 255;  
@@ -63,43 +57,25 @@ function runthis()
     var path = PathIn.text;
 	var path2 = PathOut.text;
 
-	
     var inputFolder = new Folder(path);
-	//var inputFolder = Folder.selectDialog("Select an input folder");
 	var outputFolder = new Folder(path2);
-	//var outputFolder = Folder.selectDialog("Select an output folder");
 	
 	var inputFiles = inputFolder.getFiles("*_diffuse.dds");
 	
     for(index in inputFiles)
     {
-        // open the file
 		var ModelName = GetFileName(String(inputFiles[index])).replace("_diffuse","");
 		inputFiles[index] = inputFiles[index].toString().replace("_diffuse.dds","");
-		//alert(inputFiles[index].toString().replace("_diffuse.dds",""));
-		//alert(GetFileName(String(inputFiles[index])));
-		
-        //var fileToOpen = new File(inputFiles[index] + "_diffuse.dds");
-        //open(fileToOpen);
+
 		opendds(inputFiles[index] + "_diffuse.dds");
 		var wc3_diffuse = app.activeDocument;
 		app.activeDocument.activeLayer.name = 'Layer 0';
 		var lBck =  app.activeDocument.activeLayer;
-		
-		//fileToOpen = null;
-        //file = null;
-		
-		//var fileToOpen = new File(inputFiles[index] + "_orm.dds");
-        //open(fileToOpen);
+
 		opendds(inputFiles[index] + "_orm.dds");
 		
 		var wc3_orm = app.activeDocument;
-		
-		//fileToOpen = null;
-		//file = null;
-		
-		//var fileToOpen = new File(inputFiles[index] + "_normal.dds");
-        //open(fileToOpen);
+
 		opendds(inputFiles[index] + "_normal.dds");
 		var wc3_normal = app.activeDocument;
 		
@@ -402,159 +378,13 @@ function runthis()
 
 }
 
+/******* Support functions *******/
+
 function GetFileName(fullPath)
 {
     var m = fullPath.match(/(.*)[\/\\]([^\/\\]+)\.\w+$/);
     return m[2];
 }
-
-function savedds(ddspath)
-{
-	var ddsformat = "BC3";
-
-	var idsave = charIDToTypeID( "save" );
-	var desc135 = new ActionDescriptor();
-	var idAs = charIDToTypeID( "As  " );
-	var desc136 = new ActionDescriptor();
-	var idpres = charIDToTypeID( "pres" );
-	desc136.putString( idpres, ddsformat );
-	var idIn = charIDToTypeID( "In  " );
-	desc135.putPath( idIn, new File( ddspath ) );
-	var idDocI = charIDToTypeID( "DocI" );
-	desc135.putInteger( idDocI, 1767 );
-	var idCpy = charIDToTypeID( "Cpy " );
-	desc135.putBoolean( idCpy, true );
-	executeAction( idsave, desc135, DialogModes.NO );
-	
-}
-
-function opendds(ddspath2)
-{
-	var idOpn = charIDToTypeID( "Opn " );
-	var desc6 = new ActionDescriptor();
-	var iddontRecord = stringIDToTypeID( "dontRecord" );
-	desc6.putBoolean( iddontRecord, false );
-	var idforceNotify = stringIDToTypeID( "forceNotify" );
-	desc6.putBoolean( idforceNotify, true );
-	var idnull = charIDToTypeID( "null" );
-	desc6.putPath( idnull, new File( ddspath2 ) );
-	var idAs = charIDToTypeID( "As  " );
-	var desc7 = new ActionDescriptor();
-	var idmipm = charIDToTypeID( "mipm" );
-	desc7.putBoolean( idmipm, false );
-	var idalps = charIDToTypeID( "alps" );
-	desc7.putBoolean( idalps, true );
-	var idIntelTextureWorksIntelTextureWorks = stringIDToTypeID( "Intel® Texture Works Intel® Texture Works" );
-    desc6.putObject( idAs, idIntelTextureWorksIntelTextureWorks, desc7 );
-	var idDocI = charIDToTypeID( "DocI" );
-	desc6.putInteger( idDocI, 220 );
-	executeAction( idOpn, desc6, DialogModes.NO );
-}
-
-function SelectMask()
-{	
-	var idShw = charIDToTypeID( "Shw " );
-    var desc410 = new ActionDescriptor();
-    var idnull = charIDToTypeID( "null" );
-        var list134 = new ActionList();
-            var ref235 = new ActionReference();
-            var idChnl = charIDToTypeID( "Chnl" );
-            var idOrdn = charIDToTypeID( "Ordn" );
-            var idTrgt = charIDToTypeID( "Trgt" );
-            ref235.putEnumerated( idChnl, idOrdn, idTrgt );
-        list134.putReference( ref235 );
-    desc410.putList( idnull, list134 );
-	executeAction( idShw, desc410, DialogModes.NO );
-
-	// =======================================================
-
-	var idHd = charIDToTypeID( "Hd  " );
-		var desc411 = new ActionDescriptor();
-		var idnull = charIDToTypeID( "null" );
-			var list135 = new ActionList();
-				var ref236 = new ActionReference();
-				var idChnl = charIDToTypeID( "Chnl" );
-				var idChnl = charIDToTypeID( "Chnl" );
-				var idRd = charIDToTypeID( "Rd  " );
-				ref236.putEnumerated( idChnl, idChnl, idRd );
-			list135.putReference( ref236 );
-				var ref237 = new ActionReference();
-				var idChnl = charIDToTypeID( "Chnl" );
-				var idChnl = charIDToTypeID( "Chnl" );
-				var idGrn = charIDToTypeID( "Grn " );
-				ref237.putEnumerated( idChnl, idChnl, idGrn );
-			list135.putReference( ref237 );
-				var ref238 = new ActionReference();
-				var idChnl = charIDToTypeID( "Chnl" );
-				var idChnl = charIDToTypeID( "Chnl" );
-				var idBl = charIDToTypeID( "Bl  " );
-				ref238.putEnumerated( idChnl, idChnl, idBl );
-			list135.putReference( ref238 );
-		desc411.putList( idnull, list135 );
-	executeAction( idHd, desc411, DialogModes.NO );
-}	
-
-function MakeAdjLayer()
-{
-var idMk = charIDToTypeID( "Mk  " );
-    var desc7 = new ActionDescriptor();
-    var idnull = charIDToTypeID( "null" );
-        var ref1 = new ActionReference();
-        var idAdjL = charIDToTypeID( "AdjL" );
-        ref1.putClass( idAdjL );
-    desc7.putReference( idnull, ref1 );
-    var idUsng = charIDToTypeID( "Usng" );
-        var desc8 = new ActionDescriptor();
-        var idType = charIDToTypeID( "Type" );
-            var desc9 = new ActionDescriptor();
-            var idpresetKind = stringIDToTypeID( "presetKind" );
-            var idpresetKindType = stringIDToTypeID( "presetKindType" );
-            var idpresetKindDefault = stringIDToTypeID( "presetKindDefault" );
-            desc9.putEnumerated( idpresetKind, idpresetKindType, idpresetKindDefault );
-            var idClrz = charIDToTypeID( "Clrz" );
-            desc9.putBoolean( idClrz, false );
-        var idHStr = charIDToTypeID( "HStr" );
-        desc8.putObject( idType, idHStr, desc9 );
-    var idAdjL = charIDToTypeID( "AdjL" );
-    desc7.putObject( idUsng, idAdjL, desc8 );
-executeAction( idMk, desc7, DialogModes.NO );
-}
-
-function SetAdjLayer( iHue, iSat, iLgh)
-{	
-	var idsetd = charIDToTypeID( "setd" );
-    var desc10 = new ActionDescriptor();
-    var idnull = charIDToTypeID( "null" );
-        var ref2 = new ActionReference();
-        var idAdjL = charIDToTypeID( "AdjL" );
-        var idOrdn = charIDToTypeID( "Ordn" );
-        var idTrgt = charIDToTypeID( "Trgt" );
-        ref2.putEnumerated( idAdjL, idOrdn, idTrgt );
-    desc10.putReference( idnull, ref2 );
-    var idT = charIDToTypeID( "T   " );
-        var desc11 = new ActionDescriptor();
-        var idpresetKind = stringIDToTypeID( "presetKind" );
-        var idpresetKindType = stringIDToTypeID( "presetKindType" );
-        var idpresetKindCustom = stringIDToTypeID( "presetKindCustom" );
-        desc11.putEnumerated( idpresetKind, idpresetKindType, idpresetKindCustom );
-        var idAdjs = charIDToTypeID( "Adjs" );
-            var list1 = new ActionList();
-                var desc12 = new ActionDescriptor();
-                var idH = charIDToTypeID( "H   " );
-                desc12.putInteger( idH, iHue );
-                var idStrt = charIDToTypeID( "Strt" );
-                desc12.putInteger( idStrt, iSat );
-                var idLght = charIDToTypeID( "Lght" );
-                desc12.putInteger( idLght, iLgh );
-            var idHsttwo = charIDToTypeID( "Hst2" );
-            list1.putObject( idHsttwo, desc12 );
-        desc11.putList( idAdjs, list1 );
-    var idHStr = charIDToTypeID( "HStr" );
-    desc10.putObject( idT, idHStr, desc11 );
-executeAction( idsetd, desc10, DialogModes.NO );	
-}
-
-/******* Support functions *******/
 
 function cTID(s) { return app.charIDToTypeID(s); }
 function sTID(s) { return app.stringIDToTypeID(s); }
@@ -573,4 +403,148 @@ function selectColorRange(color1, color2){
     desc.putObject( cTID("Mnm "), cTID("RGBC"), color1 ); 
     desc.putObject( cTID("Mxm "), cTID("RGBC"), color2 ); 
     executeAction( cTID("ClrR"), desc, DialogModes.NO );
+}
+
+/******* Low-level functions *******/
+
+function savedds(ddspath)
+{
+	var ddsformat = "BC3";
+	var idsave = charIDToTypeID( "save" );
+	var desc135 = new ActionDescriptor();
+	var idAs = charIDToTypeID( "As  " );
+	var desc136 = new ActionDescriptor();
+	var idpres = charIDToTypeID( "pres" );
+	desc136.putString( idpres, ddsformat );
+	var idIn = charIDToTypeID( "In  " );
+	desc135.putPath( idIn, new File( ddspath ) );
+	var idDocI = charIDToTypeID( "DocI" );
+	desc135.putInteger( idDocI, 1767 );
+	var idCpy = charIDToTypeID( "Cpy " );
+	desc135.putBoolean( idCpy, true );
+	executeAction( idsave, desc135, DialogModes.NO );
+}
+
+function opendds(ddspath2)
+{
+	var idOpn = charIDToTypeID( "Opn " );
+	var desc6 = new ActionDescriptor();
+	var iddontRecord = stringIDToTypeID( "dontRecord" );
+	desc6.putBoolean( iddontRecord, false );
+	var idforceNotify = stringIDToTypeID( "forceNotify" );
+	desc6.putBoolean( idforceNotify, true );
+	var idnull = charIDToTypeID( "null" );
+	desc6.putPath( idnull, new File( ddspath2 ) );
+	var idAs = charIDToTypeID( "As  " );
+	var desc7 = new ActionDescriptor();
+	var idmipm = charIDToTypeID( "mipm" );
+	desc7.putBoolean( idmipm, false );
+	var idalps = charIDToTypeID( "alps" );
+	desc7.putBoolean( idalps, true );
+	var idIntelTextureWorksIntelTextureWorks = stringIDToTypeID( "IntelÂ® Texture Works IntelÂ® Texture Works" );
+    desc6.putObject( idAs, idIntelTextureWorksIntelTextureWorks, desc7 );
+	var idDocI = charIDToTypeID( "DocI" );
+	desc6.putInteger( idDocI, 220 );
+	executeAction( idOpn, desc6, DialogModes.NO );
+}
+
+function SelectMask()
+{	
+	var idShw = charIDToTypeID( "Shw " );
+    var desc410 = new ActionDescriptor();
+    var idnull = charIDToTypeID( "null" );
+    var list134 = new ActionList();
+	var ref235 = new ActionReference();
+	var idChnl = charIDToTypeID( "Chnl" );
+	var idOrdn = charIDToTypeID( "Ordn" );
+	var idTrgt = charIDToTypeID( "Trgt" );
+	ref235.putEnumerated( idChnl, idOrdn, idTrgt );
+	list134.putReference( ref235 );
+    desc410.putList( idnull, list134 );
+	executeAction( idShw, desc410, DialogModes.NO );
+
+	var idHd = charIDToTypeID( "Hd  " );
+	var desc411 = new ActionDescriptor();
+	var idnull = charIDToTypeID( "null" );
+	var list135 = new ActionList();
+	var ref236 = new ActionReference();
+	var idChnl = charIDToTypeID( "Chnl" );
+	var idChnl = charIDToTypeID( "Chnl" );
+	var idRd = charIDToTypeID( "Rd  " );
+	ref236.putEnumerated( idChnl, idChnl, idRd );
+	list135.putReference( ref236 );
+	var ref237 = new ActionReference();
+	var idChnl = charIDToTypeID( "Chnl" );
+	var idChnl = charIDToTypeID( "Chnl" );
+	var idGrn = charIDToTypeID( "Grn " );
+	ref237.putEnumerated( idChnl, idChnl, idGrn );
+	list135.putReference( ref237 );
+	var ref238 = new ActionReference();
+	var idChnl = charIDToTypeID( "Chnl" );
+	var idChnl = charIDToTypeID( "Chnl" );
+	var idBl = charIDToTypeID( "Bl  " );
+	ref238.putEnumerated( idChnl, idChnl, idBl );
+	list135.putReference( ref238 );
+	desc411.putList( idnull, list135 );
+	executeAction( idHd, desc411, DialogModes.NO );
+}	
+
+function MakeAdjLayer()
+{
+	var idMk = charIDToTypeID( "Mk  " );
+    var desc7 = new ActionDescriptor();
+    var idnull = charIDToTypeID( "null" );
+	var ref1 = new ActionReference();
+	var idAdjL = charIDToTypeID( "AdjL" );
+	ref1.putClass( idAdjL );
+    desc7.putReference( idnull, ref1 );
+    var idUsng = charIDToTypeID( "Usng" );
+	var desc8 = new ActionDescriptor();
+	var idType = charIDToTypeID( "Type" );
+	var desc9 = new ActionDescriptor();
+	var idpresetKind = stringIDToTypeID( "presetKind" );
+	var idpresetKindType = stringIDToTypeID( "presetKindType" );
+	var idpresetKindDefault = stringIDToTypeID( "presetKindDefault" );
+	desc9.putEnumerated( idpresetKind, idpresetKindType, idpresetKindDefault );
+	var idClrz = charIDToTypeID( "Clrz" );
+	desc9.putBoolean( idClrz, false );
+	var idHStr = charIDToTypeID( "HStr" );
+	desc8.putObject( idType, idHStr, desc9 );
+    var idAdjL = charIDToTypeID( "AdjL" );
+    desc7.putObject( idUsng, idAdjL, desc8 );
+	executeAction( idMk, desc7, DialogModes.NO );
+}
+
+function SetAdjLayer( iHue, iSat, iLgh)
+{	
+	var idsetd = charIDToTypeID( "setd" );
+    var desc10 = new ActionDescriptor();
+    var idnull = charIDToTypeID( "null" );
+	var ref2 = new ActionReference();
+	var idAdjL = charIDToTypeID( "AdjL" );
+	var idOrdn = charIDToTypeID( "Ordn" );
+	var idTrgt = charIDToTypeID( "Trgt" );
+	ref2.putEnumerated( idAdjL, idOrdn, idTrgt );
+    desc10.putReference( idnull, ref2 );
+    var idT = charIDToTypeID( "T   " );
+	var desc11 = new ActionDescriptor();
+	var idpresetKind = stringIDToTypeID( "presetKind" );
+	var idpresetKindType = stringIDToTypeID( "presetKindType" );
+	var idpresetKindCustom = stringIDToTypeID( "presetKindCustom" );
+	desc11.putEnumerated( idpresetKind, idpresetKindType, idpresetKindCustom );
+	var idAdjs = charIDToTypeID( "Adjs" );
+	var list1 = new ActionList();
+	var desc12 = new ActionDescriptor();
+	var idH = charIDToTypeID( "H   " );
+	desc12.putInteger( idH, iHue );
+	var idStrt = charIDToTypeID( "Strt" );
+	desc12.putInteger( idStrt, iSat );
+	var idLght = charIDToTypeID( "Lght" );
+	desc12.putInteger( idLght, iLgh );
+	var idHsttwo = charIDToTypeID( "Hst2" );
+	list1.putObject( idHsttwo, desc12 );
+	desc11.putList( idAdjs, list1 );
+    var idHStr = charIDToTypeID( "HStr" );
+    desc10.putObject( idT, idHStr, desc11 );
+	executeAction( idsetd, desc10, DialogModes.NO );	
 }
